2a7a40e292044f1acc4cfd39083cd46d6c145d3a
oricalvo <ori.calvo@gmail.com>
2015-07-12 16:47:21 +0300

    More

---
+++ Samples@class/04 jQuery/01 GettingStarted/GettingStarted/App.js
@@ -0,0 +1,26 @@
+﻿/// <reference path="C:\EMC\Front End Development\Samples@class\04 jQuery\01 GettingStarted\GettingStarted\jquery.js" />
+
+$(function () {
+    console.log("DOM is ready");
+
+    var jqWrappedSet = $("button");
+    console.log(jqWrappedSet.length);
+    jqWrappedSet.text("XXX");
+});
+
+//jQuery("button").text("XXX");
+
+//var jqWrapper = jQuery(document);
+//jqWrapper.ready(function () {
+//    console.log("DOM is ready");
+//});
+
+//window.addEventListener("DOMContentLoaded", function () {
+//    console.log("DOM is ready 2");
+//});
+
+//var element = document.getElementById("button1");
+//var jqElement = jQuery("#button1");
+
+//$(function () {
+//});
---
+++ Samples@class/04 jQuery/02 Dynamic DOM/GettingStarted/App.js
@@ -0,0 +1,105 @@
+﻿$(function () {
+
+    function MainView(element) {
+        var me = this;
+
+        me.element = element;
+        me.contacts = [];
+        me.ul = this.element.find("ul");
+
+        me.element.find("buttonAdd").on("click", function () {
+            me.buttonAdd_Click();
+        });
+    }
+
+    MainView.prototype.buttonAdd_Click = function () {
+    }
+
+    var contacts = [
+        { id: 1, name: "Ori" },
+        { id: 2, name: "Roni" }
+    ];
+
+    var ul = $(".contacts");
+    var buttonAdd = $(".buttonAdd");
+    var inputName = $(".inputName");
+    var inputFile = $("input[type=file]");
+    var buttonBrowse = $(".buttonBrowse");
+
+    buttonBrowse.click(function () {
+        inputFile.trigger("click");
+    });
+
+    inputFile.on("change", function (e) {
+        if (!e.target.files.length) {
+            return;
+        }
+
+        var file = e.target.files[0];
+        console.log(file);
+    });
+
+    updateUI();
+
+    buttonAdd.on("click", function () {
+        var name = inputName.val();
+        if (!name) {
+            alert("Please specify a name");
+            return;
+        }
+
+        var contact = {
+            id: -1,
+            name: name,
+        };
+
+        contacts.push(contact);
+
+        updateUI();
+    });
+
+    function findIndex(contactId) {
+        for (var i = 0; i < contacts.length; i++) {
+            if (contacts[i].id == contactId) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    function buttonDelete_Click() {
+        var button = $(this);
+
+        //alert(button.attr("data-contactId"));
+        var contactId = button.closest("li").data("contactId");
+
+        var index = findIndex(contactId);
+        if (index != -1) {
+            contacts.splice(index, 1);
+        }
+
+        updateUI();
+    }
+
+    function updateUI() {
+        ul.empty();
+
+        contacts.forEach(function (contact) {
+            var li = $("<li/ >");
+            li.text(contact.name);
+            li.data("contactId", contact.id);
+            ul.append(li);
+
+            var button = $("<button>Delete</button>");
+            //button.attr("data-contactId", contact.id);
+
+            li.append(button);
+            button.click(buttonDelete_Click);
+        });
+
+        //$("a");
+
+        //$(".header").find("a")
+    }
+});
